fcd27b9dbf131bf2e8c2946c1ea2ac10323b7c50
Apiyconst <apiyconst@hotmail.com>
2024-08-12 00:34:34 +0300

    Greserve Apploy v1

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,30 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import Boss from './Boss';
 import Profile from '../Profile/Profile'; // Profil bileşenini import et
 
 const Game: React.FC = () => {
   const [energy, setEnergy] = useState(100);
-  const [username, setUsername] = useState<string>(""); // Kullanıcı adı state
+  const [username] = useState("PlayerOne"); // Kullanıcı adı
   const [, setTotalCoins] = useState(0); // Total coins state
-
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const userId = 'some-user-id'; // Gerçek kullanıcı ID'sini burada kullanın
-        const response = await axios.get(`https://greserver-b4a1eced30d9.herokuapp.com/userdata?id=${userId}`);
-        const userData = response.data;
-
-        // Kullanıcı adını state'e ata
-        setUsername(userData.username);
-      } catch (error) {
-        console.error('Error fetching user data:', error);
-        setUsername("PlayerOne"); // API çağrısı sırasında hata olursa varsayılan kullanıcı adı
-      }
-    };
-
-    fetchUserData();
-  }, []);
 
   const handleBossClick = () => {
     if (energy > 0) {
@@ -40,6 +21,8 @@ const Game: React.FC = () => {
     <div className="relative flex flex-col items-center justify-center min-h-screen p-4 font-orbitron">
       {/* Boss component */}
       <Boss onClick={handleBossClick} onDeath={handleBossDeath} />
+
+      {/* Boost component */}
 
       {/* Profil bileşeni */}
       <Profile username={username} />
