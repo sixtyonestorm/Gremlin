9b15e5e83123bc94d501e13c85e0d840f1e2886c
Apiyconst <apiyconst@hotmail.com>
2024-08-13 13:14:52 +0300

    Waso

--- src/components/Game/utils/popupanimations.ts
+++ src/components/Game/utils/popupanimations.ts
@@ -4,6 +4,6 @@ export const animatePopup = (element: HTMLElement) => {
   gsap.fromTo(
     element,
     { opacity: 0, scale: 0.5, rotationX: 90 },
-    { opacity: 2, scale: 1, rotationX: 0, duration: 0.6, ease: 'back.out(1.7)' }
+    { opacity: 0, scale: 1, rotationX: 0, duration: 0.6, ease: 'back.out(1.7)' }
   );
 };
