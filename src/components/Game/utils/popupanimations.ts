2744757b1606e48c9790a833a0fe4f268a016984
Apiyconst <apiyconst@hotmail.com>
2024-08-13 13:27:59 +0300

    Waso

--- src/components/Game/utils/popupanimations.ts
+++ src/components/Game/utils/popupanimations.ts
@@ -3,7 +3,7 @@ import { gsap } from 'gsap';
 export const animatePopup = (element: HTMLElement) => {
   gsap.fromTo(
     element,
-    { opacity: 0, scale: 0, rotationX: 90 },
-    { opacity: 1, scale: 1, rotationX: 0, duration: 0.6, ease: 'back.out(1.7)' }
+    { opacity: 0, scale: 1.1, rotationX: 0 },  // Başlangıç durumu: biraz büyük ve şeffaf
+    { opacity: 1, scale: 1, rotationX: 0, duration: 0.4, ease: 'ease.out' }  // Bitiş durumu: tamamen opak ve orijinal boyut
   );
 };
