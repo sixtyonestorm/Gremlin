9059929791dc3155a150408c81740ffe4b45cd5c
Apiyconst <apiyconst@hotmail.com>
2024-08-15 01:54:40 +0300

    bosses

--- src/components/Game/RewardPopup.tsx
+++ src/components/Game/RewardPopup.tsx
@@ -31,7 +31,7 @@ const RewardPopup: React.FC<RewardPopupProps> = ({ isVisible, onClose, coin, exp
         <p className="text-sm text-gray-300 mb-3 text-center">
           Boss defeated! Claim your rewards:
         </p>
-        <div className="flex justify-between items-center mb-3 bg-gray-800 p-2 rounded">
+        <div className="flex justify-between items-center mb-3 bg-green-800 p-2 rounded">
           <span className="text-yellow-400 flex items-center">
             <svg className="w-4 h-4 mr-1 fill-current" viewBox="0 0 20 20">
               <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
@@ -41,7 +41,7 @@ const RewardPopup: React.FC<RewardPopupProps> = ({ isVisible, onClose, coin, exp
           </span>
           <span className="font-semibold text-yellow-400">+{coin}</span>
         </div>
-        <div className="flex justify-between items-center mb-4 bg-gray-800 p-2 rounded">
+        <div className="flex justify-between items-center mb-4 bg-green-800 p-2 rounded">
           <span className="text-blue-400 flex items-center">
             <svg className="w-4 h-4 mr-1 fill-current" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clipRule="evenodd" />
