56a1573921730dade1a2d8bc0d59481fb8dfe612
Apiyconst <apiyconst@hotmail.com>
2024-08-15 00:57:33 +0300

    bosses

--- src/components/Game/RewardPopup.tsx
+++ src/components/Game/RewardPopup.tsx
@@ -20,29 +20,29 @@ const RewardPopup: React.FC<RewardPopupProps> = ({ isVisible, onClose, coin, exp
   if (!isVisible) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-800 bg-opacity-70">
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-800 bg-opacity-60">
       <div
         ref={popupRef}
-        className="bg-gradient-to-br from-green-900 via-green-800 to-black p-6 rounded-lg shadow-2xl max-w-sm w-full border border-green-600"
+        className="bg-gradient-to-br from-green-900 via-green-800 to-black p-4 rounded-lg shadow-lg max-w-xs w-full border border-green-500"
       >
-        <h2 className="text-2xl font-extrabold text-white mb-4 text-center">
-          Victory Achieved!
+        <h2 className="text-xl font-bold text-white mb-2 text-center">
+          Victory!
         </h2>
-        <p className="text-lg text-gray-300 mb-4 text-center">
-          Congratulations! You've defeated the boss and earned rewards.
+        <p className="text-md text-gray-300 mb-2 text-center">
+          You defeated the boss!
         </p>
-        <p className="text-lg text-gray-300 mb-4 text-center">
-          <span className="font-semibold text-yellow-400 text-2xl">{coin}</span> coins!
+        <p className="text-md text-gray-300 mb-2 text-center">
+          <span className="font-semibold text-yellow-400">{coin}</span> coins
         </p>
-        <p className="text-lg text-gray-300 mb-6 text-center">
-          <span className="font-semibold text-blue-400 text-2xl">{experience}</span> experience points!
+        <p className="text-md text-gray-300 mb-4 text-center">
+          <span className="font-semibold text-blue-400">{experience}</span> XP
         </p>
         <div className="flex justify-center">
           <button
             onClick={onClose}
-            className="px-6 py-2 bg-gradient-to-r from-yellow-400 to-yellow-600 text-black font-bold rounded-full shadow-lg hover:from-yellow-500 hover:to-yellow-700 transition duration-300 ease-in-out text-md"
+            className="px-4 py-1 bg-gradient-to-r from-yellow-400 to-yellow-600 text-black font-bold rounded-full shadow-md hover:from-yellow-500 hover:to-yellow-700 transition duration-200 ease-in"
           >
-            Awesome!
+            Close
           </button>
         </div>
       </div>
